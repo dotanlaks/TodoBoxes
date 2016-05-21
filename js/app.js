c0e530e451315b733f412f0b5ede054d02375830
Dotan Laks <dotanlaks6@gmail.com>
2016-05-21 11:26:25 +0300

    Initial commit. Includes ability to add a box of tasks and to add tasks to each box using modal window.

---
+++ js/app.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Dotana on 05/05/16.
+ */
+jQuery.noConflict();
+var app = angular.module('TODO', ['ngMaterial']);
+
+app .controller('tasksCtrl', function($scope){
+    $scope.boxes = [{boxName: "Today", tasks:[
+        {name: 'Clean', priority: 3},
+        {name: 'Study', priority: 4}]},
+        {boxName: "Today", tasks:[
+            {name: 'Clean', priority: 3},
+            {name: 'Study', priority: 4}]}];
+
+    $scope.addTaskBox = function (name, priority) {
+        if ($scope.isBoxModal == true){
+            $scope.boxes.push({boxName: name, tasks:[]});
+        }
+        else{
+            $scope.boxes[$scope.boxSelected].tasks.push({name: name, priority: priority});
+        }
+        $scope.clearModalFields();
+        $scope.closeModal();
+    }
+
+    $scope.clearModalFields = function () {
+        $scope.taskBoxName = "";
+        $scope.taskPriority = "";
+        $scope.boxSelected = 0; // is necessary?
+    }
+
+    $scope.clearAddBoxFields = function () {
+        $scope.boxName = "";
+    }
+    
+    $scope.openModal = function (boxIndex) {
+        if (boxIndex == null){
+            $scope.modalTitle = "Add Box";
+            $scope.isBoxModal = true;
+        }
+        else {
+            $scope.modalTitle = "Add Task";
+            $scope.isBoxModal = false;
+            $scope.boxSelected = boxIndex;
+        }
+        jQuery("#addTaskBoxModal").css("display", "block");
+        jQuery("#taskBoxNameId").focus();
+    }
+
+    $scope.closeModal = function () {
+        jQuery("#addTaskBoxModal").css("display", "none");
+    }
+});
